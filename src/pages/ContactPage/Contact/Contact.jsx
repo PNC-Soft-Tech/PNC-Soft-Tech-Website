d8fd010a0713eb53a02c6528b3e2fb151231e17a
anichu <anis.cse5.bu@gmail.com>
2023-07-12 11:36:14 +0600

    [DONE] contact page

--- src/pages/ContactPage/Contact/Contact.jsx
+++ src/pages/ContactPage/Contact/Contact.jsx
@@ -1,9 +1,13 @@
 import Banner from "../../../Shared/Banner/Banner";
+import Map from "../../../Shared/Map/Map";
+import Form from "../ContactForm/Form/Form";
 
 const Contact = () => {
 	return (
 		<div>
 			<Banner title="Contact Us" linkText="Contact" />
+			<Form></Form>
+			<Map longitude={90.35419886406886} latitude={22.70365780639195} />
 		</div>
 	);
 };
