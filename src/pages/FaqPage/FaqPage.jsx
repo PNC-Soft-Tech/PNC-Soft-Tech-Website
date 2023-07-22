ffdcc8a0bddb183e438c185f1d520034e1cf9eae
SobujDasCseBu <88959298+SobujDasCseBu@users.noreply.github.com>
2023-07-23 01:06:37 +0600

    Complete Faq page

--- src/pages/FaqPage/FaqPage.jsx
+++ src/pages/FaqPage/FaqPage.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import Banner from '../../Shared/Banner/Banner'
 import GenQesPart from './GenQesPart'
+import AskQuestion from './AskQuestion'
 
 const FaqPage = () => {
   return (
     <div>
         <Banner title="General Questions" linkText="Faq" />
         <GenQesPart />
+        <AskQuestion />
     </div>
   )
 }
