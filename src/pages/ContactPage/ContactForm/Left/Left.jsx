647f48cf95a05ac881b541687045659a3492f3d9
SobujDasCseBu <88959298+SobujDasCseBu@users.noreply.github.com>
2023-10-18 00:29:38 +0600

    Contact Page updated

--- src/pages/ContactPage/ContactForm/Left/Left.jsx
+++ src/pages/ContactPage/ContactForm/Left/Left.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { Colors } from "../../../../constant/colors";
 
@@ -9,6 +10,7 @@ const Left = () => {
 		subject: "",
 		message: "",
 	});
+
 
 	const { fullName, phoneNumber, email, subject, message } = formData;
 
@@ -38,7 +40,9 @@ const Left = () => {
 			<h1 className="mx-5 text-2xl font-semibold text-left mb-5 ">
 				Request a Quote
 			</h1>
-			<form onSubmit={handleSubmit}>
+			<a className="ml-5 text-blue-600 text-[30px] leading-10 font-bold " href="https://docs.google.com/forms/d/e/1FAIpQLSee22ZeYdIplLkDW2tTdUyuWjp6yyFXQY5vvCy9mMRqKzEJIg/viewform?fbclid=IwAR2dNSwoKwMylEzX8Wnxg4yNij0g0of2OMevDxZST-VvriO01XhFY0Qa6Zw">Click here to contact us</a>
+			
+			{/* <form onSubmit={handleSubmit}>
 				<div className="flex flex-col w-full sm:flex-row">
 					<div className="w-full mx-0 mb-4 sm:mx-5 sm:w-1/2">
 						<label htmlFor="fullName" className="block mb-2">
@@ -138,7 +142,7 @@ const Left = () => {
 						</button>
 					</div>
 				</div>
-			</form>
+			</form> */}
 		</div>
 	);
 };
