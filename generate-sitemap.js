acd2f65fcd13541f8982431f0c303fa6bf18bc43
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-07-26 23:56:00 +0600

    sitemap added

---
+++ generate-sitemap.js
@@ -0,0 +1,30 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const generateSitemap = async () => {
+  const sitemap = new SitemapStream({ hostname: 'https://www.pncsoft.tech' });
+  const writeStream = createWriteStream(`${__dirname}/public/sitemap.xml`);
+
+  sitemap.pipe(writeStream);
+
+  sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
+  sitemap.write({ url: '/about', changefreq: 'daily', priority: 0.1 });
+  // Add more URLs as needed
+
+  sitemap.end();
+
+  await streamToPromise(sitemap);
+};
+
+generateSitemap()
+  .then(() => {
+    console.log('Sitemap generated successfully!');
+  })
+  .catch((error) => {
+    console.error('Error generating sitemap:', error);
+  });
