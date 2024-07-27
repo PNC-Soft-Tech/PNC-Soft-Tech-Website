9e555498b1309f41e53b5a7d714a84901e42d8de
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-07-27 06:29:46 +0600

    google analytics added

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -19,6 +19,15 @@ const Home = () => {
       <meta property="og:type" content="website" />
 	  <meta name="google-site-verification" content="MWLJ5SvtAhyJ6sVDa5RvLS7PkuGJ8oE2oy9bdKMAa7c" />
 	  <meta name="keywords" content="software development,mobile app development, PNC Soft Tech, app development, ReactJS, nodejs development, bug fix, MERN stack development, Tailwindcss development SEO" />
+	  <script async src="https://www.googletagmanager.com/gtag/js?id=G-NX486BYDVM"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NX486BYDVM');
+        `}
+      </script>
 	  </Helmet>
  <ScrollToTop />
 		<Homedetails />
